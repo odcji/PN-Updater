6ec9b1d0d8695a0a31da98f7435fc838774a8308
PN Tech <pavlopavlo20055002@gmail.com>
2025-09-07 17:33:15 +0300

    feat: add main application entry point

---
+++ app.py
@@ -0,0 +1,28 @@
+import sys
+
+from ui.app_ui import Ui_MainWindow
+
+from mvc.model import Model
+from mvc.view import View
+from mvc.controller import Controller
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # MVC Initialization
+        self.model = Model()
+        self.view = View(ui=self.ui)
+        self.controller = Controller(model=self.model, view=self.view)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
