17c2481f9319daf8e1d931227b9d5d6c9477b524
PN Tech <pavlopavlo20055002@gmail.com>
2025-09-10 00:13:46 +0300

    refactor: rewrite scaling implementation

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from mvc.controller import Controller
 from classes.notifications import Notificator
 
 from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -32,17 +31,13 @@ class MyWindow(QMainWindow):
         self.is_update_available = self.controller.check_update() # Check for an update
         if self.is_update_available:
             # We call the program output
-            QTimer.singleShot(0, self.controller.update_program) 
+            # QTimer.singleShot(0, self.controller.update_program) 
+            pass
         else:
             Notificator.show_notification(notify_type="info", notify_title="Обновление", notify_text="Обновление не требуется")
             self.view.update_buttons_state(open_btn_state=True, exit_btn_state=True)
 
 if __name__ == "__main__":
-    # Turn on Hight-DPI scaling
-    QApplication.setHighDpiScaleFactorRoundingPolicy(
-        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
-    )
-
     app = QApplication(sys.argv)
 
     # Fonts for the application
